281ef6cc6060d347c23e9c2170b7433ad57404f2
Esmeralda <98198757+girlofthecodes@users.noreply.github.com>
2022-08-12 00:08:08 -0500

    Delete profiles directory

--- profiles/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from profiles import views
-
-urlpatterns = [
-    path('profiles/register/', views.UserProfileView.as_view(),),
-    path('profile/list/', views.UserProfileView.as_view(),),
-    path('profiles/update/<int:id>/', views.ProfileView.as_view(),),
-    path('profiles/delete/<int:id>/', views.ProfileView.as_view(),),
-]
