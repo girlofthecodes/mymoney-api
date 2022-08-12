f404454e0f3ed714823c4d3280f00577720fd5a8
Esmeralda <98198757+girlofthecodes@users.noreply.github.com>
2022-08-12 00:02:00 -0500

    Delete goals directory

--- goals/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from goals import views
-
-urlpatterns = [
-    path('goals/register/', views.GoalRegisterView.as_view(),),
-    path('goals/list/', views.GoalListview.as_view(),),
-    path('goals/update/<id>/', views.GoalUpdateView.as_view(),),
-    path('goals/delete/<id>/', views.GoalDeleteView.as_view(),)
-]
